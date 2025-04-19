db44f1afd82fb5fcc6b9ef5fd7907d814ee3cd33
Pawel <swinkapaw@gmail.com>
2025-04-19 19:07:38 +0200

    Final version of demographic data analyzer

--- .ipynb_checkpoints/medical_data_visualizer-checkpoint.py
+++ .ipynb_checkpoints/medical_data_visualizer-checkpoint.py
@@ -5,14 +5,14 @@ import numpy as np
 
 # 1
 df = pd.read_csv('boilerplate-medical-data-visualizer/medical_examination.csv')
-
+#print(df.head())
 # 2
 df['overweight'] = ((df['weight']/(df['height']/100) ** 2)>25).astype(int)
-
+#print(df)
 # 3
 df['cholesterol']=(df['cholesterol']>1).astype(int)
 df['gluc']=(df['gluc']>1).astype(int)
-
+print(df)
 # 4
 def draw_cat_plot():
     # 5
@@ -20,19 +20,23 @@ def draw_cat_plot():
                  id_vars=['cardio'],
                  value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-# 6
+
+
+    # 6
     df_cat = df_cat.groupby(['cardio', 'variable', 'value']) \
                .size() \
                .reset_index(name='total')
 
-# 7
+    
+    # 7
 
 
-# 8
+
+    # 8
     fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar').fig
     fig.show()
-
-# 9
+    
+    # 9
     fig.savefig('catplot.png')
     return fig
 
@@ -40,27 +44,23 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = df.copy()
-    df_heat= df_heat[
-        (df_heat['ap_lo'] <= df_heat['ap_hi']) &
-        (df_heat['height'] >= df_heat['height'].quantile(0.025)) &
-        (df_heat['height'] <= df_heat['height'].quantile(0.975)) &
-        (df_heat['weight'] >= df_heat['weight'].quantile(0.025)) &
-        (df_heat['weight'] <= df_heat['weight'].quantile(0.975))
-    ]
+    df_heat = None
 
     # 12
-    corr = df_heat.corr()
+    corr = None
 
     # 13
-    mask = np.triu(np.ones_like(corr, dtype=bool))
+    mask = None
+
+
 
     # 14
-    fig, ax = plt.subplots(figsize=(12, 10))
-    
+    fig, ax = None
+
     # 15
-    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", center=0, square=True,linewidths=.5, cbar_kws={"shrink": .5}, cmap='coolwarm')
-    fig.show()
+
+
+
     # 16
     fig.savefig('heatmap.png')
     return fig
